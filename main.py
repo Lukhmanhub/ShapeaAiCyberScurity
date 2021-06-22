c0b64f222cd634057071a77762a4a0260ce291c9
Lukhman <56676024+Lukhmanhub@users.noreply.github.com>
2021-06-22 20:36:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+from typing import TextIO
+
+import requests
+#import os
+from datetime import datetime
+
+api_key = '87d845b0b6cf29baa1a73cc34b067a95'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+import requests
+#import os
+from datetime import datetime
+
+api_key = '87d845b0b6cf29baa1a73cc34b067a95'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+with open('result.txt','w')as d:
+    print("-------------------------------------------------------------\n",file=d)
+    print("Weather Stats for - {}  || {}\n".format(location.upper(), date_time),file=d)
+    print("-------------------------------------------------------------\n",file=d)
+
+    print("Current temperature is: {:.2f} deg C".format(temp_city),file=d)
+    print("Current weather desc  :", weather_desc,file=d)
+    print("Current Humidity      :", hmdt, '%',file=d)
+    print("Current wind speed    :", wind_spd, 'kmph',file=d)
